342ed89adecf3806974533555cfda538303c07c3
Hasib98 <phoenixoffire1998@gmail.com>
2025-03-15 03:47:31 +0600

    pic

--- wp-content/themes/indian-resturant/template-homepage.php
+++ wp-content/themes/indian-resturant/template-homepage.php
@@ -85,7 +85,7 @@ $about_us_section_image = $about_section_group['about_us_section_image'];  // AC
                     <div class="count">
                         <span> <?php echo $review_count; ?></span><span><?php echo $count_unit; ?></span>
                     </div>
-                    <p class="review_text"><?php echo $review_description; ?></p>
+                    <p><?php echo $review_description; ?></p>
                 </div>
                 <div class="divider"></div>
                 <div class="review">
@@ -103,35 +103,35 @@ $about_us_section_image = $about_section_group['about_us_section_image'];  // AC
 </section>
 
 
-<section class="menu_section">
+<!-- <section class="menu_section">
     <div class="container">
-        <!-- <div class="title_group">
-            <p class="pre_title">A Indian Cuisine Restaurant</p>
-            <h1 class="title">We are the best in this food town for a decade!</h1>
-        </div>
-        <div class="food_card_container">
-            <div>
-                <img class="we_are_indian_image"
-                    src="<?php echo get_template_directory_uri() . '/assets/images/indian_food_cook.png'; ?>" alt="">
-            </div>
-            <div class="card_text">
-                <h1 class="card_title">Spicy Club</h1>
-                <p class="card_description">cIn publishing and graphic design, Lorem ipsum is a placeholder text
-                    commonly used Lorem
-                    ipsum
-                    text
-                    only.
-                </p>
-            </div>
-
-        </div> -->
         <div class="list">
             <div class="title_group">
                 <p class="pre_title">A Indian Cuisine Restaurant</p>
                 <h1 class="title">We are the best in this food town for a decade!</h1>
             </div>
             <div class="food_card_container">
-                <img src="<?php echo get_template_directory_uri() . '/assets/images/indian_food_cook.png'; ?>" alt="">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/hero_image.png'; ?>" alt="">
+                <h1 class="card_title">Spicy Club</h1>
+                <p class="card_description">In publishing and graphic design, Lorem ipsum is a placeholder text
+                    commonly used Lorem
+                    ipsum
+                    text
+                    only.
+                </p>
+            </div>
+            <div class="food_card_container">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/test.jpg'; ?>" alt="">
+                <h1 class="card_title">Spicy Club</h1>
+                <p class="card_description">cIn publishing and graphic design, Lorem ipsum is a placeholder text
+                    commonly used Lorem
+                    ipsum
+                    text
+                    only.
+                </p>
+            </div>
+            <div class="food_card_container">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/test_2.jpg'; ?>" alt="">
                 <h1 class="card_title">Spicy Club</h1>
                 <p class="card_description">cIn publishing and graphic design, Lorem ipsum is a placeholder text
                     commonly used Lorem
@@ -141,31 +141,111 @@ $about_us_section_image = $about_section_group['about_us_section_image'];  // AC
                 </p>
             </div>
 
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/indian_food_cook.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/hero_image.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/indian_food_cook.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/indian_food_cook.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/food_1.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/food_2.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/indian_food_cook.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/indian_food_cook.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/indian_food_cook.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/indian_food_cook.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/indian_food_cook.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/indian_food_cook.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/indian_food_cook.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/hero_image.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/food_1.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/indian_food_cook.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/indian_food_cook.png'; ?>" alt="">
-            <img src="<?php echo get_template_directory_uri() . '/assets/images/food_2.png'; ?>" alt="">
+            <div class="food_card_container">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/food_2.png'; ?>" alt="">
+                <h1 class="card_title">Spicy Club</h1>
+                <p class="card_description">cIn publishing and graphic design, Lorem ipsum is a placeholder text
+                    commonly used Lorem
+                    ipsum
+                    text
+                    only.
+                </p>
+            </div>
+
+            <div class="food_card_container">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/food_2.png'; ?>" alt="">
+                <h1 class="card_title">yoo suppp</h1>
+                <p class="card_description">Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+                    ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes,
+                    nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla
+                    consequat massa quis enim.</p>
+            </div>
+            <div class="food_card_container">
+                <img src="<?php echo get_template_directory_uri() . '/assets/images/indian_food_cook.png'; ?>" alt="">
+                <h1 class="card_title">Hehe boiI!!!</h1>
+                <p class="card_description">The European languages are members of the same family. Their separate
+                    existence is a myth. For science, music, sport, etc, Europe uses the same vocabulary.</p>
+            </div>
+
+
+
+
         </div>
 
     </div>
 
+</section> -->
+<section class="menu_section">
+    <div class="container">
+        <div class="list">
+            <div class="title_group">
+                <p class="pre_title">A Indian Cuisine Restaurant</p>
+                <h1 class="title">We are the best in this food town for a decade!</h1>
+            </div>
+
+            <?php
+            $food_query = new WP_Query(array(
+                'post_type'      => 'food-item', // Your custom post type
+                // 'posts_per_page' => 6, // Adjust number of posts to display
+                'order'          => 'DESC'
+            ));
+
+            if ($food_query->have_posts()) :
+                while ($food_query->have_posts()) : $food_query->the_post();
+
+                    // Get ACF fields
+                    $food_image = get_field('food_image');
+                    $food_title = get_field('food_title');
+                    $food_description = get_field('food_description');
+                    ?>
+
+            <div class="food_card_container">
+                <img src="<?php echo esc_url($food_image); ?>" alt="<?php echo esc_attr($food_title); ?>">
+                <h1 class="card_title"><?php echo esc_html($food_title); ?></h1>
+                <p class="card_description"><?php echo esc_html($food_description); ?></p>
+            </div>
+
+            <?php
+                endwhile;
+                wp_reset_postdata();
+            else :
+                echo "<p>No food items availablee.</p>";
+            endif;
+            ?>
+
+        </div>
+    </div>
 </section>
+
+
 
 <?php
 get_footer();
 
 ?>
+
+
+
+
+
+<!-- <?php
+$images = [
+    get_template_directory_uri() . '/assets/images/hero_image.png',
+    get_template_directory_uri() . '/assets/images/test_2.jpg',
+    get_template_directory_uri() . '/assets/images/food_2.png',
+    get_template_directory_uri() . '/assets/images/food_1.png',
+    get_template_directory_uri() . '/assets/images/indian_food_cook.png'
+];
+
+for ($i = 0; $i < 15; $i++) {
+    $randomImage = $images[array_rand($images)];
+    ?>
+<div class="food_card_container">
+    <img src="<?php echo $randomImage; ?>" alt="">
+    <h1 class="card_title">Spicy Club</h1>
+    <p class="card_description">In publishing and graphic design, Lorem ipsum is a placeholder text
+        commonly used Lorem ipsum text only.</p>
+</div>
+<?php
+}
+?> -->
