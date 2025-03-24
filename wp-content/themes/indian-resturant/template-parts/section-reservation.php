d3cde0401a5c307858da8e7d6679774fde2e66f9
hasib98 <phoenixoffire1998@gmail.com>
2025-03-24 10:05:52 +0600

    test

--- wp-content/themes/indian-resturant/template-parts/section-reservation.php
+++ wp-content/themes/indian-resturant/template-parts/section-reservation.php
@@ -1,30 +1,15 @@
-<section class="reservation_section">
+<?php
+    $is_reservation = is_page("reservation");
+?>
+<section class="reservation_section" <?php echo !$is_reservation ? 'style="background: url(\'' . get_template_directory_uri() . '/assets/images/reservation_bg.png\') center/cover no-repeat;"' : ''; ?>
+>
     <div class="container">
         <div class="contents">
-            <h1 style="color:black;">Online Reservation</h1>
-            <!-- <form class="reservation_form" id="reservationForm">
-                <div class="input_fields_container">
-                    <input class="reservation_inputs" type="text" id="name" placeholder="Your name" required>
-                    <input class="reservation_inputs" type="tel" id="phone" placeholder="Phone number" required>
-                    <input class="reservation_inputs" type="email" id="email" placeholder="Email Address" required>
-
-                    <select class="reservation_inputs" id="persons" name="persons">
-                        <option value="1">1 person</option>
-                        <option value="2">2 persons</option>
-                        <option value="3">3 persons</option>
-                        <option value="4">4 persons</option>
-                    </select>
-
-                    <input class="reservation_inputs" type="date" id="date" required>
-                    <input class="reservation_inputs" type="time" id="time" required>
-                </div>
-                <button class="form_submit_button" type="submit">Submit your Reservation</button>
-            </form> -->
-
-            <?php echo do_shortcode('[contact-form-7 id="4c8138c" title="reservation"]'); ?>
-
-
-
+            <h1 <?php echo $is_reservation ? 'style="color:black; text-align: center;"' : ''; ?>
+            >Online Reservation</h1>
+            <div class="reservation_form">
+                <?php echo do_shortcode('[contact-form-7 id="a59fd0f" title="Reservation"]'); ?>
+            </div>
         </div>
     </div>
 </section>
